8086e2d3a99dc8cf64a12158857fee4409e8515c
fuck you <fuck@you.com>
2021-05-13 19:44:03 -0700

    vitis synthesizes with 0 luts

---
+++ ntt.h
@@ -0,0 +1,6 @@
+#include <stdint.h>
+
+
+int32_t * ntt_red_ct_std2rev(int32_t *a);
+
+static int32_t mul_red(int32_t x, int32_t y);
---
+++ test_ntt.cpp
@@ -0,0 +1,19 @@
+#include "ntt_nina.h"
+#include <iostream>
+#define n 1024U
+
+int main() {
+	int32_t vec[n];
+
+	  for (int i = 0; i < n; i++){
+	      vec[i] = i;
+      }
+
+	ntt_red_ct_std2rev( vec);
+
+	for (int i = 0; i< n ; i++) {
+		std::cout << vec[i] << std::endl;
+	}
+ /*Downloads/ntt_rage/SRI/NTT/src/test_ntt_nina.cpp:12: undefined reference to `ntt_red_ct_std2rev(int*)*/
+
+}
